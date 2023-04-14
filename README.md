dc5d56c26f6d6e1610f5d0d3607531564a66ecae
JuJin1324 <jujin1324@daum.net>
2023-04-14 20:51:43 +0900

    #init

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring-event-starter
+
+## TODO
+### TODO
+> TODO
---
+++ src/main/java/starter/spring/event/SpringEventStarterApplication.java
@@ -0,0 +1,13 @@
+package starter.spring.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventStarterApplication.class, args);
+    }
+
+}
